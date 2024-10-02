54d798bb2e9e9f4c522af4884275a2587a6403ba
hilawnabangus <jaymaryugo90@gmail.com>
2024-10-02 10:22:46 +0800

    update

--- app/src/main/java/ph/jproyekto/todoapp/TodoListPage.kt
+++ app/src/main/java/ph/jproyekto/todoapp/TodoListPage.kt
@@ -8,13 +8,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.text.SimpleDateFormat
@@ -23,12 +29,18 @@ import java.util.Locale
 @Composable
 fun TodoListPage() {
     val todoList = getFakeTodo();
+    var inputText by remember {
+        mutableStateOf("")
+    }
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
     ) {
+        Row{
+            OutlinedTextField(value = , onValueChange = )
+        }
         LazyColumn(content = {
             itemsIndexed(todoList){
                 index: Int, item: Todo -> TodoItem(item = item)
@@ -42,11 +54,13 @@ fun TodoListPage() {
 @Composable
 fun TodoItem(item:Todo) {
     Row(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .padding(8.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(MaterialTheme.colorScheme.primary)
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ){
         Column {
             Text(
@@ -58,6 +72,14 @@ fun TodoItem(item:Todo) {
                 text = item.title,
                 fontSize = 20.sp,
                 color = Color.White
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_delete_24),
+                contentDescription = "Delete",
+                tint = Color.White
+
             )
         }
 
