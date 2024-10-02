7aef0d1e9df02018f4188b28d94953fdf3e81c52
hilawnabangus <jaymaryugo90@gmail.com>
2024-10-02 10:07:35 +0800

    update

--- app/src/main/java/ph/jproyekto/todoapp/TodoListPage.kt
+++ app/src/main/java/ph/jproyekto/todoapp/TodoListPage.kt
@@ -1,16 +1,22 @@
 package ph.jproyekto.todoapp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -35,10 +41,24 @@ fun TodoListPage() {
 
 @Composable
 fun TodoItem(item:Todo) {
-    Row{
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(16.dp)
+    ){
         Column {
-            Text(text = SimpleDateFormat("yyyy-mm-dd HH:mm", Locale.ENGLISH).format(item.createdAt))
-            Text(text = item.title)
+            Text(
+                text = SimpleDateFormat("yyyy-mm-dd HH:mm", Locale.ENGLISH).format(item.createdAt),
+                fontSize = 10.sp,
+                color = Color.White
+            )
+            Text(
+                text = item.title,
+                fontSize = 20.sp,
+                color = Color.White
+            )
         }
 
     }
