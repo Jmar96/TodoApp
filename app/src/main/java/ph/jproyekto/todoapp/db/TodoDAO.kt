1de7f76e188c84d40d209cb5f3391b75bd2743fb
hilawnabangus <jaymaryugo90@gmail.com>
2024-10-02 14:08:15 +0800

    update with DB

---
+++ app/src/main/java/ph/jproyekto/todoapp/db/TodoDAO.kt
@@ -0,0 +1,23 @@
+package ph.jproyekto.todoapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import ph.jproyekto.todoapp.Todo
+
+@Dao
+interface TodoDAO {
+
+    @Query("SELECT * FROM TODO")
+    fun getAllTodo() : LiveData<List<Todo>>
+
+    @Insert
+    fun addTodo(todo : Todo)
+
+    @Query("DELETE FROM TODO WHERE id = :id")
+    fun delete(id: Todo)
+
+
+}
